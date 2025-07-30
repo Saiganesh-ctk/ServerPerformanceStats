8158e57b0eca8724c5abf5be5344210a373c9368
Saiganesh-ctk <saiganeshctk@gmail.com>
2025-07-30 22:21:14 +0530

    added cpu usage code

--- server-status.sh
+++ server-status.sh
@@ -3,3 +3,9 @@ echo "# System Update Info #"
 echo "######################"
 
 uptime
+
+echo "##################"
+echo "#Total Cpu Usage#"
+echo "##################"
+
+top -bn1 | grep "%Cpu(s):" | cut -d ',' -f 4 | awk '{print "Usage: " 100-$1 "%"}'
